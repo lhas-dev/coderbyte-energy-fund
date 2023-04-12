712b150856e69d03a86305d55147ca3c948db893
lhas <luizhrqas@gmail.com>
2023-04-12 20:20:16 -0300

    feat: finish tab bars navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeScreen} from './screens/Home';
 import {LoginScreen} from './screens/Login';
 import {SignUpScreen} from './screens/SignUp';
 import {AssetScreen} from './screens/Asset';
+import Home from './assets/icons/home.svg';
+import Trade from './assets/icons/trade.svg';
+import Portfolio from './assets/icons/portfolio.svg';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -15,7 +19,57 @@ export type RootStackParamList = {
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
 
+const HomeTabIcon = ({focused}: {focused: boolean}) => {
+  return <Home color={focused ? '#770FDF' : '#000'} />;
+};
+
+const TradeTabIcon = ({focused}: {focused: boolean}) => {
+  return <Trade color={focused ? '#770FDF' : '#000'} />;
+};
+
+const PortfolioTabIcon = ({focused}: {focused: boolean}) => {
+  return <Portfolio color={focused ? '#770FDF' : '#000'} />;
+};
+
+function HomeTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#770FDF',
+        tabBarStyle: {
+          paddingBottom: 5,
+        },
+      }}>
+      <Tab.Screen
+        name="HomeTab"
+        options={{
+          title: 'Home',
+          tabBarIcon: HomeTabIcon,
+        }}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name="TradeTab"
+        component={HomeScreen}
+        options={{
+          title: 'Trade',
+          tabBarIcon: TradeTabIcon,
+        }}
+      />
+      <Tab.Screen
+        name="PortfolioTab"
+        component={HomeScreen}
+        options={{
+          title: 'Portfolio',
+          tabBarIcon: PortfolioTabIcon,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 function App(): JSX.Element {
   return (
     <NavigationContainer>
@@ -24,7 +78,7 @@ function App(): JSX.Element {
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeTab} />
         <Stack.Screen name="Asset" component={AssetScreen} />
       </Stack.Navigator>
     </NavigationContainer>
