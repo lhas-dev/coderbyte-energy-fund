0acbdd7ff9311460eacf56d417b4a4c355425d93
lhas <luizhrqas@gmail.com>
2023-04-12 16:19:18 -0300

    feat: initialize component screens

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export const SignUpScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUp</Text>
+    </SafeAreaView>
+  );
+};
